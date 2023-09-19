c1103615722fd7857c38b920809a9ba94716622c
tracking-app <tracking.app.co@gmail.com>
2023-09-19 13:11:06 +0300

    feat: add form html elements

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,6 +1,7 @@
-import './styles.scss';
 import { IFormProps } from './types';
 import { useEffect } from 'react';
+import LockIcon from '@mui/icons-material/Lock';
+import './styles.scss';
 
 const Form = (props: IFormProps) => {
   const { formRef, setIsVisible } = props;
@@ -33,6 +34,19 @@ const Form = (props: IFormProps) => {
       </div>
       <div className="form__input">
         <input className="form__input__item" type="text" />
+      </div>
+      <div>
+        <input type="checkbox" />
+        <span>I accept my information to be used in commercial purposes.</span>
+      </div>
+      <div>
+        <LockIcon />
+        <span>
+          Your personal information is being processed in accordance with law
+        </span>
+      </div>
+      <div>
+        <button>SEND</button>
       </div>
     </div>
   );
